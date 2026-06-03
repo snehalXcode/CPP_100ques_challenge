1c6550c0a457745e3a3f502869166330deb8075e
snehalXcode <snehalsathawane2511@gmail.com>
2026-06-03 14:33:06 +0530

    even odd number

---
+++ even_odd.cpp
@@ -0,0 +1,16 @@
+// Check whether a number is even or odd.
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a ;
+    cin >> a ;
+
+    if(a%2==0){
+        cout<<"Even";
+    }
+    else{
+        cout<<"Odd";
+    }
+    return 0;
+}
