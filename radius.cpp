0fe22a3078ccec5b370a8b97e23fba057d519baa
snehalXcode <snehalsathawane2511@gmail.com>
2026-06-03 13:02:32 +0530

    Radius of circle

---
+++ radius.cpp
@@ -0,0 +1,10 @@
+// Take radius as input and calculate area of circle.
+
+#include <iostream>
+using namespace std;    
+int main (){
+    int r;
+    cin >> r;
+    cout << "Area of circle: " << 3.14 * r * r << endl;
+    return 0;
+}
