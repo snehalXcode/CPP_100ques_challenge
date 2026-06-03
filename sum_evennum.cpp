1a2ca2b9019e499d805151c929e90441e507a8f3
snehalXcode <snehalsathawane2511@gmail.com>
2026-06-03 23:47:10 +0530

    sum of even nums

---
+++ sum_evennum.cpp
@@ -0,0 +1,17 @@
+// Find sum of first N even numbers.
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int sum = 0;
+
+    for(int i = 1 ; i <= n ; i++){
+        if(i%2 == 0){
+            sum = sum + i ;
+        }
+    }
+    cout << sum ;
+    return 0;
+}
