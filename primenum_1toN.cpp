8b128289b73e6410a3d19620fa7d37b4cb7e2b89
snehalXcode <snehalsathawane2511@gmail.com>
2026-06-03 23:38:19 +0530

    prime num upto n

---
+++ primenum_1toN.cpp
@@ -0,0 +1,14 @@
+// Print prime numbers from 1 to N.
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+
+    for(int i = 2 ; i <= n ; i++){
+        if(n%i != 0){
+            cout<<i<<" ";
+        }
+    }
+    return 0;
+}
