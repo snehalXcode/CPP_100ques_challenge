df6e060c1a6ebc5b7bbcfd234ecf93fa59d53e5b
snehalXcode <snehalsathawane2511@gmail.com>
2026-06-03 21:23:32 +0530

    GCD

---
+++ LCM.cpp
@@ -0,0 +1,9 @@
+// Find LCM of two numbers.
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin >> a >> b;
+    int lcm
+}
--- gcd.cpp
+++ gcd.cpp
@@ -2,17 +2,41 @@
 #include <iostream>
 using namespace std;
 
+// int main(){
+//     int a , b ;
+//     cin >> a >> b;
+//     while(a != b){
+//         if(a > b){
+//             a = a - b ;
+//         }
+//         else{
+//             b = b - a ;
+//         }
+//     }
+//     cout << a ;
+//     return 0;
+// // }
+// Take two numbers.
+// Find smaller number.
+// Run loop from 1 to smaller number.
+// Check:
+// a%i==0
+// b%i==0
+// If both true:
+// store i in gcd
+// After loop ends:
+// gcd contains largest common divisor.
+
 int main(){
-    int a , b ;
-    cin >> a >> b;
-    while(a != b){
-        if(a > b){
-            a = a - b ;
-        }
-        else{
-            b = b - a ;
+    int a,b;
+    cin>>a>>b;
+    int gcd = 1;
+    int smaller = (a<b) ? a : b;
+    for(int i=1; i<=smaller; i++){
+        if(a%i==0 && b%i==0){
+            gcd = i;
         }
     }
-    cout << a ;
+    cout << gcd;
     return 0;
 }
