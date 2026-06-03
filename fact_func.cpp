6eaf5dc8d666b2cfa8e2f8db70c2d3b61f164a6d
snehalXcode <snehalsathawane2511@gmail.com>
2026-06-03 23:51:11 +0530

    fact function

---
+++ fact_func.cpp
@@ -0,0 +1,18 @@
+// Find factorial using function.
+#include <iostream>
+using namespace std;
+
+int fact(int n){
+    int factorial = 1 ;
+    for(int i = 1 ; i <= n ; i++){
+        factorial = factorial * i ;
+    }
+    return factorial ;
+}
+
+int main(){
+    int n ;
+    cin >> n ;
+    cout << fact(n);
+    return 0;
+}
