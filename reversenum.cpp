a5e931b952a3fc438724925875d14d378bcbbed1
snehalXcode <snehalsathawane2511@gmail.com>
2026-06-03 15:20:13 +0530

    number reversed

---
+++ reversenum.cpp
@@ -0,0 +1,15 @@
+// Reverse a number.
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n ;
+    cin >> n;
+    int rev = 0 ;
+    while(n!=0){
+        rev = rev*10 + n%10;
+        n = n/10;
+    }
+    cout << "Reversed number: " << rev << endl;
+    return 0;
+}
