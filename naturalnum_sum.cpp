ee9830a263d8e4f2221306187d3016cdec5e5e77
snehalXcode <snehalsathawane2511@gmail.com>
2026-06-03 15:05:15 +0530

    sum of natural number

---
+++ naturalnum_sum.cpp
@@ -0,0 +1,15 @@
+// Find the sum of first N natural numbers.
+#include <iostream>
+using namespace std;
+int main (){
+    int n ;
+    cout<<"number : ";
+    cin >> n;
+    int sum = 0 ;
+
+    for(int i = 1 ; i <= n ; i++){
+        sum = sum + i ;
+    }
+    cout<<sum<<endl;
+    return 0 ;
+}
