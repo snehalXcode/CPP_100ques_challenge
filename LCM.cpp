ba2583089fac4598db5778ddd29d1519786a16a3
snehalXcode <snehalsathawane2511@gmail.com>
2026-06-03 21:30:34 +0530

    LCM

--- LCM.cpp
+++ LCM.cpp
@@ -1,9 +1,26 @@
 // Find LCM of two numbers.
+// Take two numbers.
+// Start from the larger number.
+// Keep increasing by 1.
+// Check:
+// num % a == 0
+// num % b == 0
+// If both true:
+// that's the LCM.
+// stop.
 #include <iostream>
 using namespace std;
 
 int main(){
     int a,b;
     cin >> a >> b;
-    int lcm
+    int lcm = (a > b) ? a : b;      
+    while(true){
+        if(lcm % a == 0 && lcm % b == 0){
+            cout << lcm;
+            break;
+        }
+        lcm++;
+    }
+    return 0;
 }
